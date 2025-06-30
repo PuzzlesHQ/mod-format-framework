073cb5ef9c68a2726c83a411007f4bfbd4a057d4
Mr-Zombii <thezombiigames@gmail.com>
2025-06-30 17:51:33 -0500

    add java8 compilation support & added maven central support.

--- src/main/java/dev/puzzleshq/mod/util/EntrypointPair.java
+++ src/main/java/dev/puzzleshq/mod/util/EntrypointPair.java
@@ -1,12 +1,52 @@
 package dev.puzzleshq.mod.util;
 
+import java.util.Objects;
+
 /**
  * A record of the entrypoint.
- *
- * @param entrypoint the entrypoint path.
- * @param adapter the entrypoint adapter.
  */
-public record EntrypointPair(
-        String entrypoint,
-        String adapter
-) {}
+public final class EntrypointPair {
+    private final String entrypoint;
+    private final String adapter;
+
+    /**
+     * @param entrypoint the entrypoint path.
+     * @param adapter    the entrypoint adapter.
+     */
+    public EntrypointPair(
+            String entrypoint,
+            String adapter
+    ) {
+        this.entrypoint = entrypoint;
+        this.adapter = adapter;
+    }
+
+    public String entrypoint() {
+        return entrypoint;
+    }
+
+    public String adapter() {
+        return adapter;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        EntrypointPair that = (EntrypointPair) obj;
+        return Objects.equals(this.entrypoint, that.entrypoint) &&
+                Objects.equals(this.adapter, that.adapter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(entrypoint, adapter);
+    }
+
+    @Override
+    public String toString() {
+        return "EntrypointPair[" +
+                "entrypoint=" + entrypoint + ", " +
+                "adapter=" + adapter + ']';
+    }
+}
--- src/main/java/dev/puzzleshq/mod/util/MixinConfig.java
+++ src/main/java/dev/puzzleshq/mod/util/MixinConfig.java
@@ -1,12 +1,52 @@
 package dev.puzzleshq.mod.util;
 
+import java.util.Objects;
+
 /**
  * A record of the mixin config file.
- *
- * @param path the path to the mixin config file.
- * @param environment the environment this mixin config is intended for.
  */
-public record MixinConfig(
-        String path,
-        String environment /* client, server, unknown */
-) {}
+public final class MixinConfig {
+    private final String path;
+    private final String environment;
+
+    /**
+     * @param path        the path to the mixin config file.
+     * @param environment the environment this mixin config is intended for.
+     */
+    public MixinConfig(
+            String path,
+            String environment /* client, server, unknown */
+    ) {
+        this.path = path;
+        this.environment = environment;
+    }
+
+    public String path() {
+        return path;
+    }
+
+    public String environment() {
+        return environment;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        MixinConfig that = (MixinConfig) obj;
+        return Objects.equals(this.path, that.path) &&
+                Objects.equals(this.environment, that.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path, environment);
+    }
+
+    @Override
+    public String toString() {
+        return "MixinConfig[" +
+                "path=" + path + ", " +
+                "environment=" + environment + ']';
+    }
+}
